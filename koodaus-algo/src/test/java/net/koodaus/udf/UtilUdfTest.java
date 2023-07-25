94851825be3fbd4bfc110317df1fb6aa1ddeb7b1
Maksim Zinal <maxzinal@yandex.ru>
2023-07-25 13:09:04 +0300

    test

--- koodaus-algo/src/test/java/net/koodaus/udf/UtilUdfTest.java
+++ koodaus-algo/src/test/java/net/koodaus/udf/UtilUdfTest.java
@@ -9,6 +9,11 @@ import org.junit.Test;
  */
 public class UtilUdfTest {
 
+    private static final String PASSWORD = "служил гаврило колумнистом";
+    private static final String FIO_FIRST = "Гагик Спартакович";
+    private static final String FIO_LAST = "Пагосян";
+    private static final String FIO_FULL = "Пагосян Гагик Спартакович";
+
     private static final String[] VALUES = {
         "uvI53uYUDX4AAAAAAAAAAQ",
         "sbI53uYUDX4AAAAAAAAADQ",
@@ -27,14 +32,28 @@ public class UtilUdfTest {
     };
 
     @Test
-    public void test1() {
-        final UtilUdf uu = new UtilUdf("служил гаврило колумнистом");
+    public void testLong2Str() {
+        final UtilUdf uu = new UtilUdf(PASSWORD);
         int pos = 0;
         for (long v = 1; v < 999999999999999L; v *= 13L) {
             String xv = uu.long2str(v);
             Assert.assertEquals(VALUES[pos], xv);
             pos += 1;
         }
+    }
+
+    @Test
+    public void testFirstname() {
+        final UtilUdf uu = new UtilUdf(PASSWORD);
+        String v = uu.firstname(FIO_FULL);
+        Assert.assertEquals(FIO_FIRST, v);
+    }
+
+    @Test
+    public void testLastname() {
+        final UtilUdf uu = new UtilUdf(PASSWORD);
+        String v = uu.lastname(FIO_FULL);
+        Assert.assertEquals(FIO_LAST, v);
     }
 
 }
